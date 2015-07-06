db83d12b5e8fcfb628e1bde355ce265306595cae
migranda <migranda@utpl.edu.ec>
2015-07-05 22:32:29 -0500

    Evento

--- perfiles/crear_evento.php
+++ perfiles/crear_evento.php
@@ -1,10 +1,10 @@
 <div style="width:100%; margin-bottom:2em;">
   <div style="width:90%; display:inline-block; text-align:center;">
-<<<<<<< HEAD
+
     <h2>Formulario para Crear un Nuevo Partido</h2>  
-=======
+
     <h2>Crear un Partido</h2>  
->>>>>>> origin/master
+
   </div>
   <div style="width:08%; display:inline-block; text-align:right; font-size:24px;">
     <a title="Cerrar" href="perfil.php" style="text-decoration:none; color:#585858;"><span class="glyphicon glyphicon-remove-circle"></span></a>
@@ -26,7 +26,7 @@
             </select>
           </div>
         </div>
-<<<<<<< HEAD
+
 
         <div class="form-group">
           <label for="posicion" class="col-sm-2 control-label">Fecha </label>
@@ -35,15 +35,13 @@
           </div>
         </div>
 
-
-=======
         <div class="form-group">
           <label for="posicion" class="col-sm-2 control-label">Fecha </label>
           <div class="col-sm-10">
             <input style="width:80%;"type="date" class="form-control" id="posicion" name="fecha">
           </div>
         </div>
->>>>>>> origin/master
+
         <div class="form-group">
           <div >
             <label for="posicion" class="col-sm-2 control-label">Estado </label>
@@ -61,22 +59,18 @@
         <div class="form-group">
           <label for="mail" class="col-sm-2 control-label">Nombre Equipo A</label>
           <div class="col-sm-10">
-<<<<<<< HEAD
+
             <input type="text" class="form-control" id="mail" name="equipoA" value="Equipo A"  >
-=======
-            <input type="text" class="form-control" id="mail" name="equipoA" value="Equipo A">
->>>>>>> origin/master
+
           </div>
         </div>
 
         <div class="form-group">
           <label for="mail" class="col-sm-2 control-label">Nombre Equipo B</label>
           <div class="col-sm-10">
-<<<<<<< HEAD
+
             <input type="text" class="form-control" id="mail" name="equipoB" value="Equipo B" >
-=======
-            <input type="text" class="form-control" id="mail" name="equipoB" value="Equipo B">
->>>>>>> origin/master
+
           </div>
         </div>
 
@@ -96,16 +90,16 @@
           </div>
         </div>
 
-<<<<<<< HEAD
+
 
         <?php 
           echo '<input type="hidden" class="form-control" id="owner" name="owner" value="'.$_SESSION["email"].'">';
         ?>
         <input type="hidden" name="bd" value="partidos">
 
-=======
+
         <input type="hidden" name="base" value="partidos">  
->>>>>>> origin/master
+
   <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
       <button type="submit" class="btn btn-default">Guardar</button>
