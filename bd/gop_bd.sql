e42cce0d59958a4e6601bb01115bcc0e97e68b63
Jessica Nathaly Correa Tenesaca <jncorrea@utpl.edu.ec>
2015-07-06 23:06:49 -0500

    Script base de datos

--- bd/gop_bd.sql
+++ bd/gop_bd.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-07-2015 a las 20:51:33
+-- Tiempo de generación: 07-07-2015 a las 04:58:19
 -- Versión del servidor: 5.6.21
 -- Versión de PHP: 5.6.3
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `gop_bd`
 --
+CREATE DATABASE IF NOT EXISTS `gop_bd` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `gop_bd`;
 
 -- --------------------------------------------------------
 
@@ -34,7 +36,7 @@ CREATE TABLE IF NOT EXISTS `canchas` (
   `LATITUD` char(50) DEFAULT NULL,
   `LONGITUD` char(50) DEFAULT NULL,
   `COSTO` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `canchas`
@@ -54,10 +56,18 @@ CREATE TABLE IF NOT EXISTS `convocatoria` (
 `ID_CONVOCATORIA` int(11) NOT NULL,
   `EMAIL` char(150) DEFAULT NULL,
   `ID_PARTIDO` int(11) DEFAULT NULL,
-  `EJE_X` char(6) DEFAULT NULL,
-  `EJE_Y` char(6) DEFAULT NULL,
+  `POSICION` int(11) DEFAULT NULL,
   `EQUIPO` char(150) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `convocatoria`
+--
+
+INSERT INTO `convocatoria` (`ID_CONVOCATORIA`, `EMAIL`, `ID_PARTIDO`, `POSICION`, `EQUIPO`) VALUES
+(1, 'esquezada1@utpl.edu.ec', 1, NULL, NULL),
+(2, 'jncorrea@utpl.edu.ec', 1, NULL, NULL),
+(3, 'jperez@gmail.com', 1, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -69,16 +79,15 @@ CREATE TABLE IF NOT EXISTS `grupos` (
 `ID_GRUPO` int(11) NOT NULL,
   `NOMBRE_GRUPO` char(200) NOT NULL,
   `OWNER` char(150) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `grupos`
 --
 
 INSERT INTO `grupos` (`ID_GRUPO`, `NOMBRE_GRUPO`, `OWNER`) VALUES
-(2, 'C++ que tú', 'migranda@utpl.edu.ec'),
-(9, 'Los dormidos', 'esquezada1@utpl.edu.ec'),
-(10, 'Grupo SIs', 'migranda@utpl.edu.ec');
+(1, 'UTPL-2015', 'esquezada1@utpl.edu.ec'),
+(3, 'otro grupo', 'esquezada1@utpl.edu.ec');
 
 -- --------------------------------------------------------
 
@@ -89,18 +98,18 @@ INSERT INTO `grupos` (`ID_GRUPO`, `NOMBRE_GRUPO`, `OWNER`) VALUES
 CREATE TABLE IF NOT EXISTS `grupos_miembros` (
   `EMAIL` char(150) NOT NULL,
   `ID_GRUPO` int(11) NOT NULL,
-  `estado` varchar(1) NOT NULL
+  `ESTADO` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `grupos_miembros`
 --
 
-INSERT INTO `grupos_miembros` (`EMAIL`, `ID_GRUPO`, `estado`) VALUES
-('esquezada1@utpl.edu.ec', 2, '1'),
-('esquezada1@utpl.edu.ec', 9, '1'),
-('jncorrea@utpl.edu.ec', 2, '0'),
-('migranda@utpl.edu.ec', 2, '0');
+INSERT INTO `grupos_miembros` (`EMAIL`, `ID_GRUPO`, `ESTADO`) VALUES
+('esquezada1@utpl.edu.ec', 1, '1'),
+('esquezada1@utpl.edu.ec', 3, '1'),
+('jncorrea@utpl.edu.ec', 1, '1'),
+('jperez@gmail.com', 1, '1');
 
 -- --------------------------------------------------------
 
@@ -123,9 +132,9 @@ CREATE TABLE IF NOT EXISTS `miembros` (
 --
 
 INSERT INTO `miembros` (`EMAIL`, `PASS`, `NOMBRES`, `APELLIDOS`, `CELULAR`, `POSICION`, `AVATAR`) VALUES
-('esquezada1@utpl.edu.ec', '123', 'Edgar', 'Quezada', '0990088726', '', 'user.jpeg'),
-('jncorrea@utpl.edu.ec', '456', NULL, NULL, NULL, NULL, NULL),
-('migranda@utpl.edu.ec', '789', '', 'Granda Aguilar', '0969945150', 'Delantera', NULL);
+('esquezada1@utpl.edu.ec', '123', 'Edgar', 'Quezada', '0990128372', '', 'user.jpeg'),
+('jncorrea@utpl.edu.ec', '12345', 'Jessica', 'Correa', '0991025208', 'Delantero', ''),
+('jperez@gmail.com', '12345', '', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -142,16 +151,35 @@ CREATE TABLE IF NOT EXISTS `partidos` (
   `NOMEQUIPOB` char(150) DEFAULT NULL,
   `RESESQUIPOA` int(11) DEFAULT NULL,
   `RESEQUIPOB` int(11) DEFAULT NULL,
-  `ID_GRUPO` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+  `ID_GRUPO` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `partidos`
 --
 
 INSERT INTO `partidos` (`ID_PARTIDO`, `ID_CANCHA`, `FECHA`, `ESTADO`, `NOMEQUIPOA`, `NOMEQUIPOB`, `RESESQUIPOA`, `RESEQUIPOB`, `ID_GRUPO`) VALUES
-(7, 1, '2015-07-22 21:00:00', '1', 'Equipo A', 'Equipo B', 0, 7, 2),
-(8, 2, '2015-07-08 00:00:00', '1', 'Equipo A', 'Equipo B', 0, 0, 9);
+(1, 1, '2015-07-22 21:00:00', '1', 'Equipo A', 'Equipo B', 0, 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `temp`
+--
+
+CREATE TABLE IF NOT EXISTS `temp` (
+`id_temp` int(11) NOT NULL,
+  `email_temp` varchar(150) NOT NULL,
+  `grupo_temp` varchar(30) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `temp`
+--
+
+INSERT INTO `temp` (`id_temp`, `email_temp`, `grupo_temp`) VALUES
+(4, 'nattyc0906@gmail.com', '1'),
+(5, 'migranda@utpl.edu.ec', '1');
 
 --
 -- Índices para tablas volcadas
@@ -191,7 +219,13 @@ ALTER TABLE `miembros`
 -- Indices de la tabla `partidos`
 --
 ALTER TABLE `partidos`
- ADD PRIMARY KEY (`ID_PARTIDO`), ADD KEY `FK_ALBERGA` (`ID_CANCHA`), ADD KEY `FK_FORMAN` (`ID_GRUPO`);
+ ADD PRIMARY KEY (`ID_PARTIDO`), ADD UNIQUE KEY `FK_FORMA` (`ID_GRUPO`), ADD KEY `FK_ALBERGA` (`ID_CANCHA`);
+
+--
+-- Indices de la tabla `temp`
+--
+ALTER TABLE `temp`
+ ADD PRIMARY KEY (`id_temp`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -201,22 +235,27 @@ ALTER TABLE `partidos`
 -- AUTO_INCREMENT de la tabla `canchas`
 --
 ALTER TABLE `canchas`
-MODIFY `ID_CANCHA` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `ID_CANCHA` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT de la tabla `convocatoria`
 --
 ALTER TABLE `convocatoria`
-MODIFY `ID_CONVOCATORIA` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `ID_CONVOCATORIA` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT de la tabla `grupos`
 --
 ALTER TABLE `grupos`
-MODIFY `ID_GRUPO` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+MODIFY `ID_GRUPO` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT de la tabla `partidos`
 --
 ALTER TABLE `partidos`
-MODIFY `ID_PARTIDO` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+MODIFY `ID_PARTIDO` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `temp`
+--
+ALTER TABLE `temp`
+MODIFY `id_temp` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- Restricciones para tablas volcadas
 --
@@ -240,7 +279,7 @@ ADD CONSTRAINT `FK_PERTENECE2` FOREIGN KEY (`ID_GRUPO`) REFERENCES `grupos` (`ID
 --
 ALTER TABLE `partidos`
 ADD CONSTRAINT `FK_ALBERGA` FOREIGN KEY (`ID_CANCHA`) REFERENCES `canchas` (`ID_CANCHA`),
-ADD CONSTRAINT `FK_FORMAN` FOREIGN KEY (`ID_GRUPO`) REFERENCES `grupos` (`ID_GRUPO`);
+ADD CONSTRAINT `FK_FORMA` FOREIGN KEY (`ID_GRUPO`) REFERENCES `grupos` (`ID_GRUPO`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
