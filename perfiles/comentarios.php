de6758b5a5fc9b74019715c02fa292163bb4990f
migranda <migranda@utpl.edu.ec>
2015-08-05 10:08:48 -0500

    Fixes #21

--- perfiles/comentarios.php
+++ perfiles/comentarios.php
@@ -36,7 +36,7 @@
             </div>
           </div>
           <div class="item-body">
-            <?php echo $lista_comen[1] ?>
+            <?php echo utf8_decode($lista_comen[1]); ?>
           </div>
         </div>
       <?php } ?>
