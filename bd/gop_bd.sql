7627ba9ccc77f1e3476249b4bc79d31577859f24
Jessica Nathaly Correa Tenesaca <jncorrea@utpl.edu.ec>
2015-08-04 23:16:08 -0500

    Script bd

--- bd/gop_bd.sql
+++ bd/gop_bd.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-07-2015 a las 00:08:29
+-- Tiempo de generación: 05-08-2015 a las 06:07:39
 -- Versión del servidor: 5.6.21
 -- Versión de PHP: 5.6.3
 
@@ -34,19 +34,18 @@ CREATE TABLE IF NOT EXISTS `canchas` (
   `LATITUD` char(50) DEFAULT NULL,
   `LONGITUD` char(50) DEFAULT NULL,
   `COSTO` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `canchas`
 --
 
 INSERT INTO `canchas` (`ID_CANCHA`, `NOMBRE_CANCHA`, `DIRECCION_CANCHA`, `NUM_MAX`, `LATITUD`, `LONGITUD`, `COSTO`) VALUES
-(1, 'Cancha ejemplo', 'Loja', 12, '-4.3651522', '-79.1999994', 25),
-(2, 'Cancha Sintética de la UTPL', 'Instalaciones de la Universidad Técnica Particular de Loja', 18, '-4.032945', '-79.202649', 0),
-(3, 'La pampita', 'Av. Orillas del Zamora, Loja', 12, '-3.977599', '-79.202093', 25),
-(4, 'Punto Sport', 'Azuay 11-18 y Juan JosÃ© PeÃ±a', 8, '-4.0002533', '-79.1987175', 18),
-(5, 'Maxfutbol', 'Av. 8 de Diciembre y Jaime Roldos Aguilera (Redondel de las Pitas), Loja', 12, '-3.9695182', '-79.2076872', 20),
-(6, 'Cancha Sintetica "La Isla"', 'La isla Y Puerto Rico, Quito', 14, '-3.9867941', '-79.1990528', 25);
+(1, 'La pampita', 'Av. Orillas del Zamora, Loja', 12, '-3.977599', '-79.202093', 25),
+(2, 'Punto Sport', 'Azuay 11-18 y Juan José Peña', 8, '-4.0002533', '-79.1987175', 18),
+(3, 'Maxfutbol', 'Av. 8 de Diciembre y Jaime Roldos Aguilera (Redondel de las Pitas), Loja', 12, '-3.9695182', '-79.2076872', 20),
+(4, 'Cancha Sintetica La Rdonda', 'Pifo, Quito', 14, '-0.2315431', '-78.3387828', 25);
+
 
 -- --------------------------------------------------------
 
@@ -61,15 +60,7 @@ CREATE TABLE IF NOT EXISTS `chat` (
   `message` text NOT NULL,
   `sent` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `recd` int(10) unsigned NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `chat`
---
-
-INSERT INTO `chat` (`id`, `from`, `to`, `message`, `sent`, `recd`) VALUES
-(1, 'esquezada1', 'jncorrea', 'hola', '2015-07-21 17:03:47', 1),
-(2, 'jncorrea', 'esquezada1', 'hola', '2015-07-21 17:04:13', 1);
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -83,19 +74,7 @@ CREATE TABLE IF NOT EXISTS `comentarios` (
   `id_partido` int(11) NOT NULL,
   `fecha` datetime NOT NULL,
   `comentario` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `comentarios`
---
-
-INSERT INTO `comentarios` (`id_comentario`, `email`, `id_partido`, `fecha`, `comentario`) VALUES
-(1, 'esquezada1@utpl.edu.ec', 14, '2015-07-19 01:09:09', 'Holaaa'),
-(2, 'esquezada1@utpl.edu.ec', 13, '2015-07-21 01:32:41', 'Hola...!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'),
-(3, 'esquezada1@utpl.edu.ec', 13, '2015-07-21 02:00:41', 'Quedamos con esa alineación..'),
-(4, 'jncorrea@utpl.edu.ec', 13, '2015-07-21 02:02:04', 'No me gusta la alineación :/\r\n'),
-(5, 'jperez@gmail.com', 13, '2015-07-21 02:06:40', 'Por mi esta bien..'),
-(6, 'esquezada1@utpl.edu.ec', 13, '2015-07-21 02:01:37', 'Por mi igual\r\n');
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -110,26 +89,7 @@ CREATE TABLE IF NOT EXISTS `convocatoria` (
   `POSICION` int(11) DEFAULT NULL,
   `EQUIPO` char(150) DEFAULT NULL,
   `ESTADO` varchar(5) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `convocatoria`
---
-
-INSERT INTO `convocatoria` (`ID_CONVOCATORIA`, `EMAIL`, `ID_PARTIDO`, `POSICION`, `EQUIPO`, `ESTADO`) VALUES
-(1, 'esquezada1@utpl.edu.ec', 13, 17, 'Equipo A', '1'),
-(2, 'jncorrea@utpl.edu.ec', 13, 0, '', '2'),
-(3, 'jperez@gmail.com', 13, 24, 'Equipo B', '1'),
-(4, 'esquezada1@utpl.edu.ec', 14, 24, 'Equipo B', '1'),
-(6, 'jperez@gmail.com', 14, 17, 'Equipo A', '1'),
-(7, 'esquezada1@utpl.edu.ec', 15, 21, 'Equipo B', '1'),
-(20, 'jncorrea@utpl.edu.ec', 15, 23, 'Equipo B', '1'),
-(23, 'jncorrea@utpl.edu.ec', 14, 0, '', '2'),
-(24, 'esquezada1@utpl.edu.ec', 16, 0, '', '0'),
-(25, 'jncorrea@utpl.edu.ec', 16, 0, '', '1'),
-(26, 'jperez@gmail.com', 16, 0, '', '0'),
-(27, 'jncorrea@utpl.edu.ec', 15, 0, '', '1'),
-(28, 'jperez@gmail.com', 15, 0, '', '2');
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -141,17 +101,7 @@ CREATE TABLE IF NOT EXISTS `grupos` (
 `ID_GRUPO` int(11) NOT NULL,
   `NOMBRE_GRUPO` char(200) NOT NULL,
   `OWNER` char(150) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `grupos`
---
-
-INSERT INTO `grupos` (`ID_GRUPO`, `NOMBRE_GRUPO`, `OWNER`) VALUES
-(1, 'UTPL-2015', 'esquezada1@utpl.edu.ec'),
-(3, 'otro grupo', 'esquezada1@utpl.edu.ec'),
-(4, 'grupo prueba', 'esquezada1@utpl.edu.ec'),
-(8, 'hola lola', 'esquezada1994@gmail.com');
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -165,17 +115,6 @@ CREATE TABLE IF NOT EXISTS `grupos_miembros` (
   `ESTADO` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Volcado de datos para la tabla `grupos_miembros`
---
-
-INSERT INTO `grupos_miembros` (`EMAIL`, `ID_GRUPO`, `ESTADO`) VALUES
-('esquezada1@utpl.edu.ec', 1, '1'),
-('esquezada1@utpl.edu.ec', 4, '1'),
-('jncorrea@utpl.edu.ec', 1, '1'),
-('jncorrea@utpl.edu.ec', 4, '1'),
-('jperez@gmail.com', 1, '1');
-
 -- --------------------------------------------------------
 
 --
@@ -184,7 +123,7 @@ INSERT INTO `grupos_miembros` (`EMAIL`, `ID_GRUPO`, `ESTADO`) VALUES
 
 CREATE TABLE IF NOT EXISTS `miembros` (
   `EMAIL` char(150) NOT NULL,
-  `PASS` char(25) NOT NULL,
+  `PASS` char(250) NOT NULL,
   `USER` varchar(100) NOT NULL,
   `NOMBRES` char(200) DEFAULT NULL,
   `APELLIDOS` char(200) DEFAULT NULL,
@@ -193,16 +132,6 @@ CREATE TABLE IF NOT EXISTS `miembros` (
   `AVATAR` char(10) DEFAULT NULL,
   `ESTADO` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `miembros`
---
-
-INSERT INTO `miembros` (`EMAIL`, `PASS`, `USER`, `NOMBRES`, `APELLIDOS`, `CELULAR`, `POSICION`, `AVATAR`, `ESTADO`) VALUES
-('esquezada1@utpl.edu.ec', '123', 'esquezada1', 'Edgar', 'Quezada', '0990128372', '', 'user.jpeg', 0),
-('jncorrea@utpl.edu.ec', '12345', 'jncorrea', 'Jessica', 'Correa', '0991025208', '', '', 0),
-('jperez@gmail.com', '12345', 'jperez', '', '', '', '', 'user.jpeg', 0),
-('j_perez@utpl.edu.ec', '12345', 'jperez-96', '', '', '', '', '', 0);
 
 -- --------------------------------------------------------
 
@@ -220,17 +149,7 @@ CREATE TABLE IF NOT EXISTS `partidos` (
   `NOMEQUIPOB` char(150) DEFAULT NULL,
   `RESEQUIPOA` int(11) DEFAULT NULL,
   `RESEQUIPOB` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `partidos`
---
-
-INSERT INTO `partidos` (`ID_PARTIDO`, `ID_GRUPO`, `ID_CANCHA`, `FECHA`, `ESTADO`, `NOMEQUIPOA`, `NOMEQUIPOB`, `RESEQUIPOA`, `RESEQUIPOB`) VALUES
-(13, 1, 1, '2015-07-22 00:00:00', '1', 'Equipo A', 'Equipo B', 0, 0),
-(14, 1, 1, '2015-07-17 07:05:00', '1', 'Equipo A', 'Equipo B', 0, 0),
-(15, 3, 1, '2015-07-20 00:00:00', '1', 'Equipo A', 'Equipo B', 0, 0),
-(16, 1, 2, '2015-07-21 15:16:05', '1', 'Equipo A', 'Equipo B', 0, 0);
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -242,7 +161,7 @@ CREATE TABLE IF NOT EXISTS `temp` (
 `id_temp` int(11) NOT NULL,
   `email_temp` varchar(150) NOT NULL,
   `grupo_temp` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 --
 -- Índices para tablas volcadas
@@ -310,37 +229,37 @@ ALTER TABLE `temp`
 -- AUTO_INCREMENT de la tabla `canchas`
 --
 ALTER TABLE `canchas`
-MODIFY `ID_CANCHA` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+MODIFY `ID_CANCHA` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT de la tabla `chat`
 --
 ALTER TABLE `chat`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 --
 -- AUTO_INCREMENT de la tabla `comentarios`
 --
 ALTER TABLE `comentarios`
-MODIFY `id_comentario` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+MODIFY `id_comentario` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 --
 -- AUTO_INCREMENT de la tabla `convocatoria`
 --
 ALTER TABLE `convocatoria`
-MODIFY `ID_CONVOCATORIA` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=30;
+MODIFY `ID_CONVOCATORIA` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 --
 -- AUTO_INCREMENT de la tabla `grupos`
 --
 ALTER TABLE `grupos`
-MODIFY `ID_GRUPO` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+MODIFY `ID_GRUPO` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 --
 -- AUTO_INCREMENT de la tabla `partidos`
 --
 ALTER TABLE `partidos`
-MODIFY `ID_PARTIDO` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
+MODIFY `ID_PARTIDO` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 --
 -- AUTO_INCREMENT de la tabla `temp`
 --
 ALTER TABLE `temp`
-MODIFY `id_temp` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id_temp` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 --
 -- Restricciones para tablas volcadas
 --
@@ -369,4 +288,3 @@ ADD CONSTRAINT `FK_FORMA` FOREIGN KEY (`ID_GRUPO`) REFERENCES `grupos` (`ID_GRUP
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
