d01894aeb59fc6c4f8f0b6ad1b5799dc7c401d69
jncorrea <jncorrea93@outlook.es>
2015-08-13 15:31:50 -0500

    Registrarse

--- perfiles/sugerencias.php
+++ perfiles/sugerencias.php
@@ -7,13 +7,13 @@ $miconexion->conectar($db_name,$db_host, $db_user,$db_password);
   global $notifi;
   $miconexion->consulta(" select a.id_user, g.id_grupo, g.nombre_grupo, p.id_partido, p.fecha_partido, p.estado_partido, cd.centro_deportivo, cd.num_jugadores, a.id_alineacion 
     FROM grupos g, partidos p, centros_deportivos cd, alineacion a 
-    where p.id_grupo = g.id_grupo and p.id_centro = cd.id_centro and a.id_user  = '".$_SESSION["id"]."' and a.id_partido = p.id_partido and a.estado_alineacion=2  ");
+    where p.id_grupo = g.id_grupo and p.id_centro = cd.id_centro and a.id_user  = '".$_SESSION["id"]."' and a.id_partido = p.id_partido and a.estado_alineacion='2'  ");
   $cont1=$miconexion->numregistros();
   $mail;
   if ($cont1>0) {
     $miconexion->consulta(" select a.id_user, g.id_grupo, g.nombre_grupo, p.id_partido, p.fecha_partido, p.estado_partido, cd.centro_deportivo, cd.num_jugadores, a.id_alineacion, g.nombre_grupo 
     FROM grupos g, partidos p, centros_deportivos cd, alineacion a
-    where p.id_grupo = g.id_grupo and p.id_centro = cd.id_centro and a.id_user  = '".$_SESSION["id"]."' and a.id_partido = p.id_partido and a.estado_alineacion=2 ");
+    where p.id_grupo = g.id_grupo and p.id_centro = cd.id_centro and a.id_user  = '".$_SESSION["id"]."' and a.id_partido = p.id_partido and a.estado_alineacion='2' ");
     $cont1=0;
 
     for ($i=0; $i < $miconexion->numregistros(); $i++) {
