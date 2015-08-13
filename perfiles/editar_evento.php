4680348e7e650a85ef73c2863e94261f746c2974
esquezada1 <cat4991@gmail.com>
2015-08-13 02:40:21 -0500

    migracion a nueva BD

--- perfiles/editar_evento.php
+++ perfiles/editar_evento.php
@@ -11,7 +11,7 @@ $miconexion->consulta("select * from partidos where id_partido= '".$id."' ");
   for ($i=0; $i < $miconexion->numregistros(); $i++) { 
     $lista_evento=$miconexion->consulta_lista();
   }
-$miconexion->consulta("select * from canchas ");
+$miconexion->consulta("select * from centros_deportivos ");
   for ($i=0; $i < $miconexion->numregistros(); $i++) { 
     $lista_cancha=$miconexion->consulta_lista();
   }
@@ -31,9 +31,9 @@ $miconexion->consulta("select * from canchas ");
   <div class="form-group">
     <label for="cancha" class="col-sm-2 control-label">Cancha: </label>
     <div class="col-sm-9">
-      <select style="border-radius:5px;" name="id_cancha" value="<?php echo $lista_cancha[1] ?>" class="form-control">
+      <select style="border-radius:5px;" name="id_centro" value="<?php echo $lista_cancha[1] ?>" class="form-control">
       <?php 
-          $miconexion->consulta("select * from canchas");
+          $miconexion->consulta("select * from centros_deportivos");
           $miconexion->opciones();
       ?>
      </select>
@@ -45,8 +45,8 @@ $miconexion->consulta("select * from canchas ");
     <div class="col-sm-9">
       <p id="datepairExample">
           <input type="hidden" class="date start" name="fehhcha" placeholder="no" required /> 
-          <input type="text" class="date start" id="timeformatExample" name="fecha" value="<?php echo $lista_evento[3] ?>" required/> Hora
-          <input type="text" class="time start" id="timeformatExample" name="hora" data-scroll-default="23:30:00"  value="<?php echo $lista_evento[4] ?>" required/>
+          <input type="text" class="date start" id="timeformatExample" name="fecha_partido" value="<?php echo $lista_evento[6] ?>" required/> Hora
+          <input type="text" class="time start" id="timeformatExample" name="hora_partido" data-scroll-default="23:30:00"  value="<?php echo $lista_evento[7] ?>" required/>
       </p>
     </div>
   </div>
@@ -67,28 +67,28 @@ $miconexion->consulta("select * from canchas ");
   <div class="form-group">
     <label for="nombres" class="col-sm-2 control-label">Estado</label>
     <div class="col-sm-9">
-      <input type="text" class="form-control" id="nombres" name="estado" value="<?php echo $lista_evento[5] ?>">
+      <input type="text" class="form-control" id="nombres" name="estado_partido" value="<?php echo $lista_evento[12] ?>">
     </div>
   </div>  
   <div class="form-group">
     <label for="apellidos" class="col-xs-12 col-sm-2 control-label">Equipos</label>
     <div class="col-xs-5 col-sm-4">
-      <input type="text" class="form-control" id="mail" name="NOMEQUIPOA" value="<?php echo $lista_evento[6] ?>" >
+      <input type="text" class="form-control" id="mail" name="equipo_a" value="<?php echo $lista_evento[8] ?>" >
     </div>
     <label for="posicion" class="col-xs-1 col-sm-1 control-label">vs.</label>
     <div class="col-xs-5 col-sm-4">
-      <input type="text" class="form-control" id="mail" name="NOMEQUIPOB" value="<?php echo $lista_evento[7] ?>">
+      <input type="text" class="form-control" id="mail" name="equipo_b" value="<?php echo $lista_evento[9] ?>">
     </div>
   </div>
 
   <div class="form-group">
     <label for="posicion" class="col-xs-12 col-sm-2 control-label">Resultados</label>
     <div class="col-xs-5 col-sm-4">
-      <input type="text" class="form-control" id="mail"name="resequipoa" value="<?php echo $lista_evento[8] ?>">
+      <input type="text" class="form-control" id="mail"name="res_a" value="<?php echo $lista_evento[10] ?>">
     </div>
     <label for="posicion" class="col-xs-1 col-sm-1 control-label">- </label>
     <div class="col-xs-5 col-sm-4">
-      <input type="text" class="form-control" id="mail" name="resequipob" value="<?php echo $lista_evento[9] ?>">
+      <input type="text" class="form-control" id="mail" name="res_b" value="<?php echo $lista_evento[11] ?>">
     </div>
   </div>
 
