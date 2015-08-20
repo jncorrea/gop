cc3e6f41a358d1c2c4bfe386309383ffc83ce7d9
jncorrea <jncorrea93@outlook.es>
2015-08-20 11:02:00 -0500

    Fixes #29

--- include/insertar_evento.php
+++ include/insertar_evento.php
@@ -10,43 +10,48 @@
 	@$val="";
     @$col="";
 	@$list;
-	for ($i=0; $i <count($_POST)-2; $i++) {
-        if ($i == 4) {
-            $val[$i]=date("Y-m-d",strtotime(array_values($_POST)[$i]));
+	for ($i=1; $i <count($_POST); $i++) {
+        if ($i == 5) {
+            $val[$i-1]=date("Y-m-d",strtotime(array_values($_POST)[$i]));
         }else{
-		  $val[$i]=utf8_decode(array_values($_POST)[$i]);            
+		  $val[$i-1]=utf8_decode(array_values($_POST)[$i]);            
         }
-	}
-    for ($i=0; $i <count($_POST)-2; $i++) {
-          $col[$i]=array_keys($_POST)[$i];
+        $col[$i-1]=array_keys($_POST)[$i];
     }	
     $sql=$miconexion->ingresar_sql($bd,$col,$val);
     $insert;
-    if($miconexion->consulta($sql)){
-        $miconexion->consulta("select MAX(id_partido) AS id FROM partidos");
-        $id=$miconexion->consulta_lista();
-        $miconexion->consulta("select id_user FROM user_grupo where id_grupo='".$_POST['id_grupo']."'");
-        for ($i=0; $i < $miconexion->numregistros(); $i++) { 
-            $list=$miconexion->consulta_lista();
-            if ($list[0]==$_POST['id_user']) {
-                $insert[$i]="insert into alineacion values ('','".$id[0]."','".$list[0]."','','','','".date('Y-m-d H:i:s', time())."','1')";
-            }else{
-                $insert[$i]="insert into alineacion values ('','".$id[0]."','".$list[0]."','','','','".date('Y-m-d H:i:s', time())."','0')";
-            }
-        }
-        
-        for ($i=0; $i < count($insert); $i++) { 
-            $miconexion->consulta($insert[$i]);
-        }
+    if ($_POST['nombre_partido']=='' || $_POST['id_grupo']=='' || $_POST['id_centro']==''&&$_POST['fecha_partido']&&$_POST['hora_partido']) {
         echo '<script>
-            $container = $("#container_notify_ok").notify();    
-            create("default", { title:" Notificaci&oacute;n", text:"Partido Creado con &eacute;xito"});
-            location.href = "perfil.php?op=alineacion&id='.$id[0].'";
+                $container = $("#container_notify_bad").notify();   
+                create("default", { title:"Alerta", text:"* Campos requeridos"}); 
             </script>';
     }else{
-        echo '<script>
-            $container = $("#container_notify_bad").notify();   
-            create("default", { title:"Alerta", text:"'.$sql.'"}); 
-        </script>';
-	}
+        if($miconexion->consulta($sql)){
+            $miconexion->consulta("select MAX(id_partido) AS id FROM partidos");
+            $id=$miconexion->consulta_lista();
+            $miconexion->consulta("select id_user FROM user_grupo where id_grupo='".$_POST['id_grupo']."'");
+            for ($i=0; $i < $miconexion->numregistros(); $i++) { 
+                $list=$miconexion->consulta_lista();
+                if ($list[0]==$_POST['id_user']) {
+                    $insert[$i]="insert into alineacion values ('','".$id[0]."','".$list[0]."','','','','".date('Y-m-d H:i:s', time())."','1')";
+                }else{
+                    $insert[$i]="insert into alineacion values ('','".$id[0]."','".$list[0]."','','','','".date('Y-m-d H:i:s', time())."','0')";
+                }
+            }
+            
+            for ($i=0; $i < count($insert); $i++) { 
+                $miconexion->consulta($insert[$i]);
+            }
+            echo '<script>
+                $container = $("#container_notify_ok").notify();    
+                create("default", { title:" Notificaci&oacute;n", text:"Partido Creado con &eacute;xito"});
+                location.href = "perfil.php?op=alineacion&id='.$id[0].'";
+                </script>';
+        }else{
+            echo '<script>
+                $container = $("#container_notify_bad").notify();   
+                create("default", { title:"Alerta", text:"No se pudo crear tu partido. <br>Intenta nuevamente."}); 
+            </script>';
+    	}
+    }
 ?>
--- perfiles/crear_evento.php
+++ perfiles/crear_evento.php
@@ -17,100 +17,100 @@
       <h3 class="page-title">
         Partidos <small>Crear Partido</small>
       </h3>
-      <div class="portlet light ">       
-
-<form method="post" action="" id="form_crear_evento" enctype="multipart/form-data" class="form-horizontal">
-  <div class="form-group">
-    <label for="Nombre_Partido" class="col-xs-12 col-sm-2 control-label">Nombre del Partido:</label>
-    <div class="col-sm-9" style="padding-top:12px;">
-      <input type="text" class="form-control" id="nombre_partido" name="nombre_partido" placeholder="Da un nombre al partido..">
-    </div>
-  </div>
-    <div class="form-group">
-    <label for="Descripcion" class="col-xs-12 col-sm-2 control-label">Descripci&oacute;n:</label>
-    <div class="col-sm-9">
-      <textarea type="text" class="form-control" id="descripcion_partido" name="descripcion_partido" placeholder="Describe t&uacute; partido.."></textarea>
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="grupo" class="col-sm-2 control-label">Grupo</label>
-    <div class="col-xs-5 col-sm-4">
-      <select style="border-radius:5px;" name="id_grupo" class="form-control">
-    <?php                 
-      $miconexion->consulta("select id_grupo, nombre_grupo from grupos where id_user='".$_SESSION["id"]."' ");
-      $miconexion->opciones();
-    ?>
-    </select>
-    </div>
-    <label for="cancha" class="col-sm-1 control-label">Cancha </label>
-    <div class="col-xs-5 col-sm-4">
-      <select style="border-radius:5px;" name="id_centro" class="form-control">
-      <?php 
-          $miconexion->consulta("select id_centro, centro_deportivo from centros_deportivos");
-          $miconexion->opciones();
-      ?>
-     </select>
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="Fecha" class="col-xs-12 col-sm-2 control-label">Fecha: </label>
-    <div class="col-xs-5 col-sm-4" id="datepairExample">
-      <input type="text" class="date start form-control" name="fecha_partido" placeholder="yyyy-mm-dd" min="08-10-2015" required />
-    </div>
-    <label for="Hora" class="col-xs-1 col-sm-1 control-label">Hora: </label>
-    <div class="col-xs-5 col-sm-4">
-      <input type="text" class="time start form-control" id="timeformatExample" name="hora_partido" data-scroll-default="23:30:00" placeholder="00:00:00" required/>
-    </div>
-  </div>
-  <article>                      
-    <script>                
-        $('#datepairExample .date').datepicker({
-            'format': 'yyyy-m-d',
-            'autoclose': true
-        });
-    </script> 
-    <script>
-        $(function() {
-            $('#timeformatExample').timepicker({ 'timeFormat': 'H:i:s' });  
-        });
-    </script>
-  </article>
-
-
-  <div class="form-group">
-    <label for="estado" class="col-sm-2 control-label">Estado </label>
-    <div class="col-sm-9">
-      <label class="css-switch" style="height:33px;">
-          <input type="checkbox" name="estado_partido" value="1" class="css-switch-check" required>
-          <span class="css-switch-label"></span>
-          <span class="css-switch-handle"></span>
-      </label>
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="equipoA" class="col-xs-12 col-sm-2 control-label">Equipos</label>
-    <div class="col-xs-5 col-sm-4">
-      <input type="text" class="form-control" id="equipoA" name="equipo_a" value="Equipo A"  >
-    </div>
-    <label for="equipoB" class="col-xs-1 col-sm-1 control-label">vs. </label>
-    <div class="col-xs-5 col-sm-4">
-      <input type="text" class="form-control" id="equipoB" name="equipo_b" value="Equipo B"  >
-    </div>
-  </div>
-  <div class="form-group">
-    <input type="hidden" class="form-control" id="ResEquipoA" name="res_A" value="0" readonly>
-    <input type="hidden" class="form-control" id="ResEquipoB" name="res_B" value="0" readonly>
-    <input type="hidden" name="id_user" value="<?php echo $_SESSION['id']; ?>">
-    <input type="hidden" name="bd" value="partidos">
-  </div>
-
-</form>
-  <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
-      <button type="submit" class="btn btn-default" onclick='enviar_form("../include/insertar_evento.php","form_crear_evento");'>Guardar</button>
-    </div>
-  </div>
-  <div id="respuesta"></div>
+      <div class="portlet light ">
+        <div class="portlet-title">
+          <div class="caption">
+            <i class="icon-bubble font-red-sunglo"></i>
+            <span class="caption-subject bold uppercase" style="color: #006064;">
+              NUEVO PARTIDO
+            </span>
+            <br><span style="color: red; font-size:11px; padding:10px;">
+              * Campos requeridos
+            </span>
+          </div>
+        </div>
+        <div class="portlet-body" id="chats">
+          <div class="tab-content"> 
+            <!-- CANCHA INFO TAB -->
+            <form method="post" action="" id="form_crear_evento" enctype="multipart/form-data" class="form-horizontal">
+              <input type="hidden" name="bd" value="partidos">
+              <div class="form-group">
+                <label for="Nombre_Partido" class="col-xs-12 col-sm-2 control-label" required><span style="color:red;">* </span>Nombre del Partido:</label>
+                <div class="col-sm-9" style="padding-top:12px;">
+                  <input type="text" class="form-control" id="nombre_partido" name="nombre_partido" placeholder="Da un nombre al partido..">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="Descripcion" class="col-xs-12 col-sm-2 control-label">Descripci&oacute;n:</label>
+                <div class="col-sm-9">
+                  <textarea type="text" class="form-control" id="descripcion_partido" name="descripcion_partido" placeholder="Describe tu partido.."></textarea>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="grupo" class="col-xs-12 col-sm-2 control-label"><span style="color:red;">* </span>Grupo</label>
+                <div class="col-sm-9">
+                  <select style="border-radius:5px;" name="id_grupo" class="form-control">
+                  <?php                 
+                    $miconexion->consulta("select id_grupo, nombre_grupo from grupos where id_user='".$_SESSION["id"]."' ");
+                    $miconexion->opciones();
+                  ?>
+                  </select>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="cancha" class="col-xs-12 col-sm-2 control-label"><span style="color:red;">* </span>Lugar: </label>
+                <div class="col-sm-9">
+                  <select style="border-radius:5px;" name="id_centro" class="form-control">
+                  <?php 
+                      $miconexion->consulta("select id_centro, centro_deportivo from centros_deportivos");
+                      $miconexion->opciones();
+                  ?>
+                 </select>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="Fecha" class="col-xs-12 col-sm-2 control-label"><span style="color:red;">* </span>Cuando: </label>
+                <div class="col-xs-12 col-sm-4" id="datepairExample">
+                  <input type="text" class="date start form-control" name="fecha_partido" placeholder="yyyy-mm-dd" min="08-10-2015" required />
+                </div>
+                <label for="Hora" class="col-xs-12 col-sm-2 control-label"><span style="color:red;">* </span>Hora: </label>
+                <div class="col-xs-12 col-sm-3">
+                  <input type="text" class="time start form-control" id="timeformatExample" name="hora_partido" data-scroll-default="23:30:00" placeholder="00:00:00" required/>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="equipoA" class="col-xs-12 col-sm-2 control-label">Equipos</label>
+                <div class="col-xs-5 col-sm-4">
+                  <input type="text" class="form-control" id="equipoA" name="equipo_a" value="Equipo A"  >
+                </div>
+                <label for="equipoB" class="col-xs-1 col-sm-1 control-label">vs. </label>
+                <div class="col-xs-5 col-sm-4">
+                  <input type="text" class="form-control" id="equipoB" name="equipo_b" value="Equipo B"  >
+                </div>
+              </div>
+              <article>                      
+                <script>                
+                    $('#datepairExample .date').datepicker({
+                        'format': 'yyyy-m-d',
+                        'autoclose': true
+                    });
+                </script> 
+                <script>
+                    $(function() {
+                        $('#timeformatExample').timepicker({ 'timeFormat': 'H:i:s' });  
+                    });
+                </script>
+              </article>                                   
+            </form>
+            <div class="form-group">
+              <div class="margin-top-10 col-sm-10" style="float:right;">
+                <button type="submit" class="btn green-haze" style="background:#4CAF50;" onclick='enviar_form("../include/insertar_evento.php","form_crear_evento");'>Guardar</button>
+                <div id="respuesta"></div>
+              </div>
+            </div>           
+            <!-- END CANCHA INFO TAB -->    
+          </div>
+        </div>
       </div>
     </div>
     <div class="chat page-sidebar-menu col-lg-2 col-md-2 col-sm-12 col-xs-12" style="border-left: 1px solid #EEEEEE;">
