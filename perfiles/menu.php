3e4d67e0911e450846a276852a86314043f31624
jncorrea <jncorrea93@outlook.es>
2015-08-28 15:07:38 -0500

    Cambio del menu

--- perfiles/menu.php
+++ perfiles/menu.php
@@ -54,15 +54,15 @@ session_start();
 </li>
 <li>
 	<a href="javascript:;">
-	<i class="icon-plus-sign-alt"></i>
-	<span class="title">Operaciones</span>
+	<i class="icon-group"></i>
+	<span class="title">Mis Grupos</span>
 	<span class="arrow "></span>
 	</a>
 	<ul class="sub-menu">
-		<li>
-			<a title="Crear Grupo" style='font-size:15px; display: inline-block; padding-right:5px;' href="#" onclick="mostrar('crearGrupo'); return false" >
-        	<i class="icon-plus"></i> Crear Grupo</a>
-        	<div id="crearGrupo" style="display:none;">
+    <li>
+      <a title="Crear Grupo" style='font-size:15px; display: inline-block; padding-right:5px;' href="#" onclick="mostrar('crearGrupo'); return false" >
+          <i class="icon-plus"></i> Crear Grupo</a>
+          <div id="crearGrupo" style="display:none;">
             <form method="post" action=""class="form-horizontal" id="form_grupo" style="display:inline-block; width:65%;">
               <div class="form-horizontal" style="display:inline-block; padding-left:10px; width:100%;">
                   <input type="hidden" class="form-control" id="bd" name="bd" value="grupos">
@@ -76,34 +76,8 @@ session_start();
               <input  onclick='enviar_form("../include/insertarGrupo.php","form_grupo");' id="crear_grupo" style="width:100%; display:inline-block; text-align:center;" disabled="false" type="submit" class="btn btn-default" value="Crear">
             </div>
             <div id="resultado"></div>
-  		</div>
-		</li>
-		<li>
-      <?php 
-          $miconexion->consulta("select * from grupos where id_user='".$_SESSION['id']."'");  
-          $miconexion->numregistros();
-          if ($miconexion->numregistros()>0) {
-       ?>
-          <a title="Crear Partido" style='font-size:15px; display: inline-block; padding-right:5px;' href="perfil.php?op=crear_evento">
-          <i class="icon-plus"></i> Crear Partido</a>
-      <?php 
-          }else{
-       ?>
-          <a title="Crear Partido" style='font-size:15px; display: inline-block; padding-right:5px;' href="#" onclick="mensaje();">
-          <i class="icon-plus"></i> Crear Partido</a>
-       <?php 
-          }
-        ?>
-		</li>	
-	</ul>
-</li>
-<li>
-	<a href="javascript:;">
-	<i class="icon-group"></i>
-	<span class="title">Mis Grupos</span>
-	<span class="arrow "></span>
-	</a>
-	<ul class="sub-menu">
+      </div>
+    </li>
 		<?php
       	$miconexion->consulta("select g.nombre_grupo, g.id_grupo, g.id_user, gm.id_user from grupos g, user_grupo gm where g.id_grupo=gm.id_grupo and gm.id_user='".$_SESSION['id']."'");
       	$cont = $miconexion->numcampos();
@@ -137,6 +111,23 @@ session_start();
 	<span class="arrow "></span>
 	</a>
 	<ul class="sub-menu">
+    <li>
+      <?php 
+          $miconexion->consulta("select * from grupos where id_user='".$_SESSION['id']."'");  
+          $miconexion->numregistros();
+          if ($miconexion->numregistros()>0) {
+       ?>
+          <a title="Crear Partido" style='font-size:15px; display: inline-block; padding-right:5px;' href="perfil.php?op=crear_evento">
+          <i class="icon-plus"></i> Crear Partido</a>
+      <?php 
+          }else{
+       ?>
+          <a title="Crear Partido" style='font-size:15px; display: inline-block; padding-right:5px;' href="#" onclick="mensaje();">
+          <i class="icon-plus"></i> Crear Partido</a>
+       <?php 
+          }
+        ?>
+    </li>
 		<?php
         	$miconexion->consulta("select p.id_grupo, p.id_partido, p.fecha_partido, p.hora_partido, p.estado_partido, p.nombre_partido
         		FROM partidos p, alineacion a
