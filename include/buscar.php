26bc37670f994a0332b7d606001493034b5fc1e4
migranda <migranda@utpl.edu.ec>
2015-09-13 20:50:28 -0500

    varios

---
+++ include/buscar.php
@@ -0,0 +1,12 @@
+<?php
+	$new_row['a']="hola";
+    $new_row['a']="hola";
+    $new_row['a']="hola";
+    $new_row['a']="hola";
+
+        $row_set[] = $new_row; //build an array*/
+
+    echo "holaa";
+
+  
+?>
