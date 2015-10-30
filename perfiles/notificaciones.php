85f91db35dc328d86b794e6274c3699d9adb0777
jncorrea <jncorrea93@outlook.es>
2015-10-30 05:52:05 -0500

    Redirigir notificaciones a calendario en caso de reserva

--- perfiles/notificaciones.php
+++ perfiles/notificaciones.php
@@ -14,6 +14,11 @@ for ($i=0; $i < $miconexion->numregistros(); $i++) {
   $datos=$miconexion->consulta_lista();
   $partidos[$datos[0]]=$datos[1]; 
 }
+$miconexion->consulta("select distinct(n.id_partido), p.id_centro from notificaciones n, partidos p where n.mensaje LIKE '%ha solicitado reservar%' and n.id_partido = p.id_partido and n.id_user = '".$_SESSION['id']."'");
+for ($i=0; $i < $miconexion->numregistros(); $i++) { 
+  $datos=$miconexion->consulta_lista();
+  $centros[$datos[0]]=$datos[1]; 
+}
 ?>
   <a href="javascript:;" title="Notificaciones" class="dropdown-toggle" data-toggle="dropdown" data-close-others="true" onclick="actualizar_notificacion('16');">
   <i class="icon-bell"></i>
@@ -41,48 +46,68 @@ for ($i=0; $i < $miconexion->numregistros(); $i++) {
                 }else{
 
                   if ($notificaciones[8]!=null) {
-              if ($notificaciones[1]!="") {
-                echo "<li>
-                  <a href='perfil.php?op=grupos&id=".$notificaciones[8]."' onclick='actualizar_notificacion(".$act.",".$notificaciones[10].")' style='text-decoration:none;'><div class='col-lg-2 col-md-2 col-sm-2 col-xs-2' style='padding-left:0px;'><img style='width:30px; height:30px;' src='images/".$notificaciones[0].$notificaciones[1]."'/></div>
-                  <div style='text-align:justify;'><strong> ".$notificaciones[0]." </strong>".utf8_decode($notificaciones[5])." <strong>".$grupos[$notificaciones[8]]."</strong></div></a>
-                </li>";
-              }else{
-                if ($notificaciones[2]=="Masculino") {
-                  echo "<li>
-                  <a href='perfil.php?op=grupos&id=".$notificaciones[8]."' onclick='actualizar_notificacion(".$act.",".$notificaciones[10].")' style='text-decoration:none;'><div class='col-lg-2 col-md-2 col-sm-2 col-xs-2' style='padding-left:0px;'><img style='width:30px; height:30px;' src='../assets/img/user_masculino.png'/></div>
-                  <div style='text-align:justify;'><strong> ".$notificaciones[0]." </strong>".utf8_decode($notificaciones[5])." <strong>".$grupos[$notificaciones[8]]."</strong></div></a>
-                </li>"; 
-                }elseif ($notificaciones[2]=="Femenino") {
-                  echo "<li>
-                  <a href='perfil.php?op=grupos&id=".$notificaciones[8]."' onclick='actualizar_notificacion(".$act.",".$notificaciones[10].")' style='text-decoration:none;'><div class='col-lg-2 col-md-2 col-sm-2 col-xs-2' style='padding-left:0px;'><img style='width:30px; height:30px;' src='../assets/img/user_femenino.png'/></div>
-                  <div style='text-align:justify;'><strong> ".$notificaciones[0]." </strong>".utf8_decode($notificaciones[5])." <strong>".$grupos[$notificaciones[8]]."</strong></div></a>
-                </li>";
-                }
-              } 
-            }elseif ($notificaciones[9]!=null) {
-              if ($notificaciones[1]!="") {
-                echo "<li>
-                  <a href='perfil.php?op=alineacion&id=".$notificaciones[9]."' onclick='actualizar_notificacion(".$act.",".$notificaciones[10].")' style='text-decoration:none;'><div class='col-lg-2 col-md-2 col-sm-2 col-xs-2' style='padding-left:0px;'><img style='width:30px; height:30px;' src='images/".$notificaciones[0].$notificaciones[1]."'/></div>
-                  <div style='text-align:justify;'><strong> ".$notificaciones[0]." </strong>".utf8_decode($notificaciones[5])." <strong>".$partidos[$notificaciones[9]]."</strong></div></a>
-                </li>";
-              }else{
-                if ($notificaciones[2]=="Masculino") {
-                  echo "<li>
-                  <a href='perfil.php?op=alineacion&id=".$notificaciones[9]."' onclick='actualizar_notificacion(".$act.",".$notificaciones[10].")' style='text-decoration:none;'><div class='col-lg-2 col-md-2 col-sm-2 col-xs-2' style='padding-left:0px;'><img style='width:30px; height:30px;' src='../assets/img/user_masculino.png'/></div>
-                  <div style='text-align:justify;'><strong> ".$notificaciones[0]." </strong>".utf8_decode($notificaciones[5])." <strong>".$partidos[$notificaciones[9]]."</strong></div></a>
-                </li>"; 
-                }elseif ($notificaciones[2]=="Femenino") {
-                  echo "<li>
-                  <a href='perfil.php?op=alineacion&id=".$notificaciones[9]."' onclick='actualizar_notificacion(".$act.",".$notificaciones[10].")' style='text-decoration:none;'><div class='col-lg-2 col-md-2 col-sm-2 col-xs-2' style='padding-left:0px;'><img style='width:30px; height:30px;' src='../assets/img/user_femenino.png'/></div>
-                  <div style='text-align:justify;'><strong> ".$notificaciones[0]." </strong>".utf8_decode($notificaciones[5])." <strong>".$partidos[$notificaciones[9]]."</strong></div></a>
-                </li>";
-                }
-              } 
-            }
-
-                }
-
-            
+                    if ($notificaciones[1]!="") {
+                      echo "<li>
+                        <a href='perfil.php?op=grupos&id=".$notificaciones[8]."' onclick='actualizar_notificacion(".$act.",".$notificaciones[10].")' style='text-decoration:none;'><div class='col-lg-2 col-md-2 col-sm-2 col-xs-2' style='padding-left:0px;'><img style='width:30px; height:30px;' src='images/".$notificaciones[0].$notificaciones[1]."'/></div>
+                        <div style='text-align:justify;'><strong> ".$notificaciones[0]." </strong>".utf8_decode($notificaciones[5])." <strong>".$grupos[$notificaciones[8]]."</strong></div></a>
+                      </li>";
+                    }else{
+                      if ($notificaciones[2]=="Masculino") {
+                        echo "<li>
+                        <a href='perfil.php?op=grupos&id=".$notificaciones[8]."' onclick='actualizar_notificacion(".$act.",".$notificaciones[10].")' style='text-decoration:none;'><div class='col-lg-2 col-md-2 col-sm-2 col-xs-2' style='padding-left:0px;'><img style='width:30px; height:30px;' src='../assets/img/user_masculino.png'/></div>
+                        <div style='text-align:justify;'><strong> ".$notificaciones[0]." </strong>".utf8_decode($notificaciones[5])." <strong>".$grupos[$notificaciones[8]]."</strong></div></a>
+                      </li>"; 
+                      }elseif ($notificaciones[2]=="Femenino") {
+                        echo "<li>
+                        <a href='perfil.php?op=grupos&id=".$notificaciones[8]."' onclick='actualizar_notificacion(".$act.",".$notificaciones[10].")' style='text-decoration:none;'><div class='col-lg-2 col-md-2 col-sm-2 col-xs-2' style='padding-left:0px;'><img style='width:30px; height:30px;' src='../assets/img/user_femenino.png'/></div>
+                        <div style='text-align:justify;'><strong> ".$notificaciones[0]." </strong>".utf8_decode($notificaciones[5])." <strong>".$grupos[$notificaciones[8]]."</strong></div></a>
+                      </li>";
+                      }
+                    } 
+                  }elseif ($notificaciones[9]!=null) {
+                    $mensaje = substr($notificaciones[5], 0, 23);
+                    $comp_m = strnatcasecmp ($mensaje, "ha solicitado reservar");
+                    if ($comp_m == 0){
+                      if ($notificaciones[1]!="") {
+                        echo "<li>
+                          <a href='perfil.php?op=canchas&x=calendar&id=".$centros[$notificaciones[9]]."' onclick='actualizar_notificacion(".$act.",".$notificaciones[10].")' style='text-decoration:none;'><div class='col-lg-2 col-md-2 col-sm-2 col-xs-2' style='padding-left:0px;'><img style='width:30px; height:30px;' src='images/".$notificaciones[0].$notificaciones[1]."'/></div>
+                          <div style='text-align:justify;'><strong> ".$notificaciones[0]." </strong>".utf8_decode($notificaciones[5])." <strong>".$partidos[$notificaciones[9]]."</strong></div></a>
+                        </li>";
+                      }else{
+                        if ($notificaciones[2]=="Masculino") {
+                          echo "<li>
+                          <a href='perfil.php?op=canchas&x=calendar&id=".$centros[$notificaciones[9]]."' onclick='actualizar_notificacion(".$act.",".$notificaciones[10].")' style='text-decoration:none;'><div class='col-lg-2 col-md-2 col-sm-2 col-xs-2' style='padding-left:0px;'><img style='width:30px; height:30px;' src='../assets/img/user_masculino.png'/></div>
+                          <div style='text-align:justify;'><strong> ".$notificaciones[0]." </strong>".utf8_decode($notificaciones[5])." <strong>".$partidos[$notificaciones[9]]."</strong></div></a>
+                        </li>"; 
+                        }elseif ($notificaciones[2]=="Femenino") {
+                          echo "<li>
+                          <a href='perfil.php?op=canchas&x=calendar&id=".$centros[$notificaciones[9]]."' onclick='actualizar_notificacion(".$act.",".$notificaciones[10].")' style='text-decoration:none;'><div class='col-lg-2 col-md-2 col-sm-2 col-xs-2' style='padding-left:0px;'><img style='width:30px; height:30px;' src='../assets/img/user_femenino.png'/></div>
+                          <div style='text-align:justify;'><strong> ".$notificaciones[0]." </strong>".utf8_decode($notificaciones[5])." <strong>".$partidos[$notificaciones[9]]."</strong></div></a>
+                        </li>";
+                        }
+                      }
+                    }else{
+                      if ($notificaciones[1]!="") {
+                        echo "<li>
+                          <a href='perfil.php?op=alineacion&id=".$notificaciones[9]."' onclick='actualizar_notificacion(".$act.",".$notificaciones[10].")' style='text-decoration:none;'><div class='col-lg-2 col-md-2 col-sm-2 col-xs-2' style='padding-left:0px;'><img style='width:30px; height:30px;' src='images/".$notificaciones[0].$notificaciones[1]."'/></div>
+                          <div style='text-align:justify;'><strong> ".$notificaciones[0]." </strong>".utf8_decode($notificaciones[5])." <strong>".$partidos[$notificaciones[9]]."</strong></div></a>
+                        </li>";
+                      }else{
+                        if ($notificaciones[2]=="Masculino") {
+                          echo "<li>
+                          <a href='perfil.php?op=alineacion&id=".$notificaciones[9]."' onclick='actualizar_notificacion(".$act.",".$notificaciones[10].")' style='text-decoration:none;'><div class='col-lg-2 col-md-2 col-sm-2 col-xs-2' style='padding-left:0px;'><img style='width:30px; height:30px;' src='../assets/img/user_masculino.png'/></div>
+                          <div style='text-align:justify;'><strong> ".$notificaciones[0]." </strong>".utf8_decode($notificaciones[5])." <strong>".$partidos[$notificaciones[9]]."</strong></div></a>
+                        </li>"; 
+                        }elseif ($notificaciones[2]=="Femenino") {
+                          echo "<li>
+                          <a href='perfil.php?op=alineacion&id=".$notificaciones[9]."' onclick='actualizar_notificacion(".$act.",".$notificaciones[10].")' style='text-decoration:none;'><div class='col-lg-2 col-md-2 col-sm-2 col-xs-2' style='padding-left:0px;'><img style='width:30px; height:30px;' src='../assets/img/user_femenino.png'/></div>
+                          <div style='text-align:justify;'><strong> ".$notificaciones[0]." </strong>".utf8_decode($notificaciones[5])." <strong>".$partidos[$notificaciones[9]]."</strong></div></a>
+                        </li>";
+                        }
+                      }
+                    } 
+                  }
+                }            
           } 
         ?>
       </ul>
